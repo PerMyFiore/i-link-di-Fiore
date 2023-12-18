582ecc3fb3de76954ef038328f9e2719947f0996
PerMyFiore <153445485+PerMyFiore@users.noreply.github.com>
2023-12-18 05:16:10 +0100

    Added randomizer

--- src/Items.js
+++ src/Items.js
@@ -4,15 +4,20 @@ import './Items.css';
 const Items = () => {
   const [items, setItems] = useState([]);
 
+  const randomizeItems = () => {
+    setItems(items => [...items].sort(() => Math.random() - 0.5));
+  };
+
   useEffect(() => {
     fetch('https://raw.githubusercontent.com/PerMyFiore/PerMyFiore.github.io/main/items.json')
       .then(response => response.json())
-      .then(data => setItems(data))
+      .then(data => setItems(data.sort(() => Math.random() - 0.5)))
       .catch(error => console.error('Error:', error));
   }, []);
 
   return (
     <div>
+        <button className="FioreButton" onClick={randomizeItems}>Randomize</button>
         {items.map((item, index) => (
           <div key={index}>
             <a href={item.url}>{item.name}</a><br/>
